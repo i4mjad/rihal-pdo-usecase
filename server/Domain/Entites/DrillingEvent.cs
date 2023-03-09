8f65ae8a4071bd2a3316502e65e9da54e151d27c
Amjad Khalfan <i4mjad@gmail.com>
2023-03-09 19:23:48 +0400

    refactor, rename the property name for the event type to match

--- server/Domain/Entites/DrillingEvent.cs
+++ server/Domain/Entites/DrillingEvent.cs
@@ -2,16 +2,16 @@ namespace Domain.Entites;
 
 public class DrillingEvent
 {
-    public DrillingEvent(Guid id, double startDepth, double endDepth, EventTypes eventTypesType)
+    public DrillingEvent(Guid id, double startDepth, double endDepth, EventTypes eventType)
     {
         Id = id;
         StartDepth = startDepth;
         EndDepth = endDepth;
-        EventTypesType = eventTypesType;
+        EventType = eventType;
     }
 
     public Guid Id { get; init; }
     public double StartDepth { get; set; }
     public double EndDepth { get; set; }
-    public EventTypes EventTypesType { get; set; }
+    public EventTypes EventType { get; set; }
 }
